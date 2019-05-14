7055dc4a0791173e9d1c6a4060ec84b66cfeeb27
Sammy Barasa <barsamms@gmail.com>
2019-05-14 16:21:57 +0300

    Initial commit

---
+++ src/main/kotlin/com/acai/akilimo/AkilimoApplication.kt
@@ -0,0 +1,11 @@
+package com.acai.akilimo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AkilimoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AkilimoApplication>(*args)
+}
---
+++ src/test/kotlin/com/acai/akilimo/AkilimoApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.acai.akilimo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class AkilimoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
