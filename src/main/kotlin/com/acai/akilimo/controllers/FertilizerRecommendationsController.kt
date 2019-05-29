7aba417bec4d9f8aad807bb1222dfc0432b98087
Sammy Barasa <barsamms@gmail.com>
2019-05-29 10:06:51 +0300

    Update to return style

--- src/main/kotlin/com/acai/akilimo/controllers/FertilizerRecommendationsController.kt
+++ src/main/kotlin/com/acai/akilimo/controllers/FertilizerRecommendationsController.kt
@@ -8,7 +8,9 @@ import com.acai.akilimo.service.RecommendationServiceImp
 import org.modelmapper.ModelMapper
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.http.HttpStatus
 
 
 @RequestMapping("/api/v2/recommendations")
@@ -26,17 +28,23 @@ constructor(private val recommendationServiceImp: RecommendationServiceImp) {
     }
 
     @PostMapping("/fertilizer")
-    fun request(@RequestBody recommendationRequest: RecommendationRequestDto): RecommendationResponseDto? {
+    fun request(@RequestBody recommendationRequest: RecommendationRequestDto): ResponseEntity<RecommendationResponseDto> {
         val modelMapper = ModelMapper()
+        var recommendationResponseDto: RecommendationResponseDto? = null
 
         val request = modelMapper.map(recommendationRequest, RecommendationRequest::class.java)
 
         val response = recommendationServiceImp.saveRecommendationRequest(request!!)
-        if (response != null) {
-            logger.info("Returning a response after logging")
-            return modelMapper.map(response, RecommendationResponseDto::class.java)
+
+        return when {
+            response != null -> {
+                logger.info("Returning a response after logging")
+                recommendationResponseDto = modelMapper.map(response, RecommendationResponseDto::class.java)
+
+                ResponseEntity(recommendationResponseDto, HttpStatus.OK)
+            }
+            else -> ResponseEntity(recommendationResponseDto, HttpStatus.NOT_FOUND)
         }
 
-        return null
     }
 }
