24d69ee90684257528aa4d4dd915a80e10f7bf1e
Sammy Barasa <barsamms@gmail.com>
2019-08-21 16:06:30 +0300

    refactored sms sending payload creation

--- src/main/kotlin/com/acai/akilimo/service/MessagingService.kt
+++ src/main/kotlin/com/acai/akilimo/service/MessagingService.kt
@@ -5,17 +5,13 @@ import com.acai.akilimo.interfaces.IMessagingService
 import com.acai.akilimo.mapper.RecommendationResponseDto
 import com.acai.akilimo.properties.MessagingProperties
 import com.google.i18n.phonenumbers.Phonenumber
-import infobip.api.client.SendSingleTextualSms
 import infobip.api.config.BasicAuthConfiguration
 import java.util.*
-import infobip.api.model.sms.mt.send.textual.SMSTextualRequest
-import infobip.api.model.sms.mt.send.SMSResponse
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import com.google.i18n.phonenumbers.PhoneNumberUtil
 import infobip.api.model.Destination
 import infobip.api.model.sms.mt.send.Message
-import retrofit.RetrofitError
 import infobip.api.model.sms.mt.send.textual.SMSAdvancedTextualRequest
 import infobip.api.client.SendMultipleTextualSmsAdvanced
 import org.slf4j.LoggerFactory
@@ -61,68 +57,33 @@ constructor(akilimoConfigProperties: AkilimoConfigProperties) : IMessagingServic
         val destination = Destination()
         destination.to = response.fullPhoneNumber
 
+        val message = Message()
+        message.from = infobipSms.sender
+        message.destinations = Collections.singletonList(destination)
         if (response.fertilizerRecText != null) {
-            val message = Message()
-            message.from = infobipSms.sender
-            message.destinations = Collections.singletonList(destination)
             message.text = response.fertilizerRecText
             message.notifyUrl = webHookUrl
             messageList.add(message)
         }
 
         if (response.interCroppingRecText != null) {
-            val message = Message()
-            message.from = infobipSms.sender
-            message.destinations = Collections.singletonList(destination)
             message.text = response.interCroppingRecText
             message.notifyUrl = webHookUrl
             messageList.add(message)
         }
 
         if (response.plantingPracticeRecText != null) {
-            val message = Message()
-            message.from = infobipSms.sender
-            message.destinations = Collections.singletonList(destination)
             message.text = response.plantingPracticeRecText
             message.notifyUrl = webHookUrl
             messageList.add(message)
         }
 
         if (response.scheduledPlantingRecText != null) {
-            val message = Message()
-            message.from = infobipSms.sender
-            message.destinations = Collections.singletonList(destination)
             message.text = response.scheduledPlantingRecText
             message.notifyUrl = webHookUrl
             messageList.add(message)
         }
         return messageList
-    }
-
-    fun sendTextMessageOld(response: RecommendationResponseDto): SMSResponse? {
-        val auth = BasicAuthConfiguration(infobipSms.userName, infobipSms.userPass)
-
-        val message = response.fertilizerRecText;
-        val client = SendSingleTextualSms(auth)
-        val phoneNumber = processPhoneNumber(response)
-
-        val isValid = phoneUtil.isValidNumber(phoneNumber) // returns true
-        if (isValid) {
-            val phoneNumberString = convertToInternationalNumber(response)
-            val requestBody = SMSTextualRequest()
-            requestBody.from = infobipSms.sender
-            requestBody.to = Arrays.asList(phoneNumberString)
-            requestBody.text = message
-
-            return try {
-                client.execute(requestBody)
-            } catch (ex: RetrofitError) {
-                logger.error(ex.message)
-                return null
-            }
-        }
-        logger.info("This phone number appears to be invalid ${response.mobileNumber}")
-        return null
     }
 
     override fun sendEmailMessage(response: RecommendationResponseDto, email: Boolean) {
@@ -141,8 +102,8 @@ constructor(akilimoConfigProperties: AkilimoConfigProperties) : IMessagingServic
 
     fun convertToInternationalNumber(recommendationResponseDto: RecommendationResponseDto): String? {
 
-        val swissNumberProto = processPhoneNumber(recommendationResponseDto)
+        val phoneNumber = processPhoneNumber(recommendationResponseDto)
 
-        return phoneUtil.format(swissNumberProto, PhoneNumberUtil.PhoneNumberFormat.E164)
+        return phoneUtil.format(phoneNumber, PhoneNumberUtil.PhoneNumberFormat.E164)
     }
 }
