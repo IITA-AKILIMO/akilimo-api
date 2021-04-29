6a05bafdb1428e477837cef87d86e38cd02a8cb3
Tsobu Developer <dev@tsobu.co.ke>
2021-04-29 12:14:17 +0300

    feat: migrations for app request stats
    added migrations for app request stats
    
    BREAKING CHANGE: database migrations

---
+++ src/main/resources/liquibase/changelog/scripts/create_v_app_request_stats_view.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE VIEW v_app_request_stats_view AS
+
+SELECT app_report.created_at                    as request_date,
+       app_report.device_token,
+       app_report.country_code,
+       app_report.lat,
+       app_report.lon,
+       app_report.full_names,
+       app_report.gender                        AS genderName,
+       CASE
+           WHEN gender = 'Male' THEN 'M'
+           WHEN gender = 'Mwanaume' THEN 'M'
+           WHEN gender = 'Female' THEN 'F'
+           WHEN gender = 'Mwanamke' THEN 'F'
+           ELSE 'NA'
+           END                                  AS gender,
+       app_report.phone_number,
+       IFNULL(user_feedback.user_type, 'OTHER') as user_type,
+       CASE
+           WHEN fr = 1 THEN 'FR'
+           WHEN pp = 1 THEN 'PP'
+           WHEN ic = 1 THEN 'IC'
+           WHEN spp = 1 or sph = 1 THEN 'SPHS'
+           ELSE 'NA'
+           END
+                                                AS use_case
+
+FROM app_report
+         LEFT JOIN
+     user_feedback ON app_report.device_token = user_feedback.device_token
