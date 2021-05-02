e0a3cc55be49103c2d5db3cb244023d34f521ebc
Sammy Barasa <barsamms@gmail.com>
2021-05-03 00:57:50 +0300

    fix: corrected trigger calls naming

--- migrations/src/main/resources/liquibase/changelog/scripts/create_exclusion_flag_evaluation_update_trigger.sql
+++ migrations/src/main/resources/liquibase/changelog/scripts/create_exclusion_flag_evaluation_update_trigger.sql
@@ -1,10 +1,10 @@
-CREATE TRIGGER update_exclusion_list
+CREATE TRIGGER update_exclusion_list_update
     BEFORE UPDATE
     ON app_report
     FOR EACH ROW
 BEGIN
     DECLARE exclude_me TINYINT;
-    call update_exclusion_list(NEW.phone_number,NEW.full_names,@excludeFlag);
+    call exclusion_flag_evaluation(NEW.phone_number,NEW.full_names,@excludeFlag);
 
     select @excludeFlag into exclude_me;
 
