a4a6f4eaafcf7e220ff0a43121f2130592904e90
Sammy Barasa <s.barasa@cgiar.org>
2022-06-30 15:14:21 +0300

    feat: added server list

--- api/src/main/kotlin/com/iita/akilimo/api/config/OpenApiConfig.kt
+++ api/src/main/kotlin/com/iita/akilimo/api/config/OpenApiConfig.kt
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
 import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -21,23 +22,20 @@ class OpenApiConfig {
         @Value("\${akilimo.version}") appVersion: String,
         @Value("\${akilimo.name}") appName: String,
         @Value("\${akilimo.license}") license: String,
+        @Value("\${akilimo.dev-server}") devServer: String,
         @Value("\${akilimo.license_url}") licenseUrl: String,
         @Value("\${akilimo.developer.name}") developer: String,
         @Value("\${akilimo.developer.email}") developerEmail: String,
         @Value("\${akilimo.developer.url}") developerUrl: String,
     ): OpenAPI {
-        return OpenAPI().info(
-            Info().title(appName)
-                .description(appName)
-                .contact(
-                    Contact()
-                        .email(developerEmail)
-                        .name(developer)
-                        .url(developerUrl)
-                )
-                .version(appVersion)
-                .license(License().name(license).url(licenseUrl))
+        val servers = mutableListOf<Server>()
+        servers.add(Server().url(devServer))
+        servers.add(Server().url("https://api.akilimo.org"))
 
-        ).security(Collections.singletonList(SecurityRequirement().addList("key")))
+        val apiInfo = Info().title(appName).description(appName).contact(
+            Contact().email(developerEmail).name(developer).url(developerUrl)
+        ).version(appVersion).license(License().name(license).url(licenseUrl))
+
+        return OpenAPI().info(apiInfo).security(Collections.singletonList(SecurityRequirement().addList("key"))).servers(servers)
     }
 }
