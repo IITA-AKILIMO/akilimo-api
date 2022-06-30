9ada79c7253ca187dd68f979ffec94a3c7b539fd
Sammy Barasa <s.barasa@cgiar.org>
2022-06-30 08:25:39 +0300

    feat: added multi auth library for future testing

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -15,8 +15,7 @@ tasks.getByName<Jar>("jar") {
 }
 
 dependencies {
-    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.2")
     val coroutinesVersion = "1.6.1-native-mt"
 
     implementation(project(":config"))
--- core/src/main/kotlin/com/iita/akilimo/core/auth/SecurityConfig.kt
+++ core/src/main/kotlin/com/iita/akilimo/core/auth/SecurityConfig.kt
@@ -1,36 +1,114 @@
 package com.iita.akilimo.core.auth
 
+import com.forgerock.spring.security.multiauth.configurers.MultiAuthenticationCollectorConfigurer
+import com.forgerock.spring.security.multiauth.configurers.collectors.APIKeyCollector
+import com.forgerock.spring.security.multiauth.configurers.collectors.CustomJwtCookieCollector
+import com.forgerock.spring.security.multiauth.configurers.collectors.StaticUserCollector
 import com.iita.akilimo.database.repos.UserAuthRepo
+import com.nimbusds.jose.JWSObject
+import com.nimbusds.jose.JWSVerifier
+import com.nimbusds.jose.crypto.MACVerifier
+import com.nimbusds.jwt.JWTParser
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.annotation.Order
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
+import java.util.stream.Collectors
+import java.util.stream.Stream
 import javax.sql.DataSource
 
 @Configuration
-@EnableWebSecurity
-@Order(1)
+//@EnableWebSecurity
+//@Order(1)
 class SecurityConfig(
     val encoder: PasswordEncoder, val dataSource: DataSource, val authRepo: UserAuthRepo
 ) : WebSecurityConfigurerAdapter() {
 
+    val myLogger: Logger = LoggerFactory.getLogger(this::class.java)
+
     private val API_KEY_AUTH_HEADER_NAME = "key"
 
 
-//    @Throws(Exception::class)
-//    override fun configure(auth: AuthenticationManagerBuilder) {
-//        auth.authenticationProvider()
-//    }
+    @Throws(java.lang.Exception::class)
+    fun configureTrial(http: HttpSecurity) {
 
+        val nonAuthPaths = arrayOf(
+            "/api/v2/recommendations/**/**"
+        )
+
+        http
+            .authorizeRequests()
+            .anyRequest()
+            .permitAll()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //We don't need sessions to be created.
+            .and()
+            .apply(myAuthAdapter())
+
+//        http.cors().and().authorizeRequests().antMatchers(*nonAuthPaths).permitAll()
+//            .anyRequest()
+//            .authenticated()
+//            .and()
+//            .sessionManagement()
+//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //We don't need sessions to be created.
+//            .and()
+//            .apply(myAuthAdapter())
+//
+        http.formLogin().disable()
+//
+        http.csrf().disable()
+    }
+
+    private fun myAuthAdapter(): MultiAuthenticationCollectorConfigurer<HttpSecurity> {
+
+        /**
+         * Authentication & authorisation via a cookie 'SSO'
+         * The authorities are extracted from the 'group' claim
+         * The username is extracted from the 'sub' claim
+         * Note: JWT cookies expected to be signed with HMAC with "Qt5y2isMydGwVuREoIomK9Ei70EoFQKH0GpcbtJ4" as a secret
+         */
+        val jwTCollector = CustomJwtCookieCollector.builder().collectorName("Cookie-SSO").authoritiesCollector { token ->
+            token.jwtClaimsSet.getStringListClaim("group").stream().map { g -> SimpleGrantedAuthority(g) }
+                .collect(Collectors.toSet()) as Set<GrantedAuthority>?
+        }.tokenValidator { tokenSerialised ->
+            val jwsObject = JWSObject.parse(tokenSerialised)
+            val verifier: JWSVerifier = MACVerifier("Qt5y2isMydGwVuREoIomK9Ei70EoFQKH0GpcbtJ4")
+            jwsObject.verify(verifier)
+            JWTParser.parse(tokenSerialised)
+        }.cookieName("SSO").build()
+
+
+        val apiKeyCollector =
+            APIKeyCollector.builder<User>().collectorName("API-Key").apiKeyExtractor { req -> req.getHeader("key") }.apiKeyValidator { apiKey ->
+                User(
+                    "bob", "", Stream.of(SimpleGrantedAuthority("repo-42")).collect(Collectors.toSet())
+                )
+            }.usernameCollector(User::getUsername).authoritiesCollector { user -> HashSet<GrantedAuthority>(user.authorities) }.build()
+
+        /**
+         * Static authentication
+         * If no authentication was possible with the previous collector, we default to the anonymous user
+         */
+        val staticCollector = StaticUserCollector.builder().collectorName("StaticUser-anonymous").usernameCollector { "anonymous" }.build()
+
+        return MultiAuthenticationCollectorConfigurer<HttpSecurity>()
+            .collectorForAuthorzation(jwTCollector)
+            .collectorForAuthentication(apiKeyCollector)
+//            .collector(staticCollector)
+    }
 
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
@@ -72,13 +150,9 @@ class SecurityConfig(
             "/api/**/potato**", "/api/**/potato**/**",
             "/api/**/starch**",
             "/api/**/user-feedback**", "/api/**/user-feedback**/**",
-        ).permitAll()
-            .antMatchers(HttpMethod.GET, "/api").hasRole("ADMIN")
-            .antMatchers(HttpMethod.POST, "/api/**").hasAnyRole("ADMIN", "USER")
-            .antMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
-            .antMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
-            .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER").and()
-            .sessionManagement()
+        ).permitAll().antMatchers(HttpMethod.GET, "/api").hasRole("ADMIN").antMatchers(HttpMethod.POST, "/api/**").hasAnyRole("ADMIN", "USER")
+            .antMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN").antMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
+            .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER").and().sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //We don't need sessions to be created.
             .and().formLogin().disable()
 
